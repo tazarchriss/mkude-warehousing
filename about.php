bfabc6ef43ed93c04572fcfb1fb828842af0f331
TazarChriss <77143206+tazarchriss@users.noreply.github.com>
2021-08-16 12:39:26 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Inventory</title>
+</head>
+<body>
+    <div class="header">
+        <h1 id="nhead" style="text-align: left;">Mkude Warehouse 
+        <a href="login.php" class="signout"style="font: size 24px;"><i style="float: right;" class="fa fa-sign-in">SIGN IN</i></a>
+         <br>
+         </h1>
+         <div class="navbar">
+            <a href="index.php" id="nbar">HOME</a><a href="about.php" id="nbar">ABOUT</a> <a href="contact.php" id="nbar">CONTACT US</a>
+         </div>
+        
+      </div>
+    <div class="s-layout">
+   
+     <div class="header">
+        
+        </div>
+        <div class="col-12 col-s-12 intro" align="center">
+            <h1 style="font-size: 32px; background-color: brown;color: aliceblue;">ABOUT US</h1>
+         <div class="col-12 col-s-12 services" style="background-color:#192b3c ;color: aliceblue;">
+            
+            <P style="font-size: 20px;"><img src="team.jpg" style="float: right;width: 40%;height: 300px;">
+                Mkude Warehousing is a full-service warehouse that has been providing customer-focused storage solutions in Dar es salaam since 2005.
+                <br>
+<br>
+Our clean, secure, and well-maintained facilities, as well as our reputation for exceptional service, will ensure your goods are in safe hands.
+
+This family business operates with integrity and considers community involvement as one of its top priorities.
+
+            </P>
+         </div>
+         <br>
+         <div class="col-s-12 col-12 main2"></div>
+         <h1 style="font-size: 32px; background-color: brown;color: aliceblue;">OUR TEAM</h1>
+        
+        
+         <div class="col-2 col-s-2 team">
+            <img src="manager.jpg" alt=""style="height:80%;width: 100%;">
+            <p>
+                Peter Grewal, <br>
+                 General Manager
+            </p>
+         </div>
+         <div class="col-2 col-s-2 team">
+            <img src="assistant.jpg" alt=""style="height:80%;width: 100%;">
+            <p>
+                Tara Heigh, 
+                <br> Administrative Assistant
+            </p>
+         </div>
+         <div class="col-2 col-s-2 team">
+            <img src="supervisor.jpg" alt=""style="height:80%;width: 100%;">
+            <p>
+                Pat Brown, 
+                <br> Warehouse Supervisor
+            </p>
+         </div>
+         <div class="col-2 col-s-2 team">
+            <img src="eco.jpg" alt=""style="height:80%;width: 100%;">
+            <p>
+                Michael Priest
+                <br> E-Commerce Coordinator
+            </p>
+         </div>
+         
+         <div style="clear: both;"></div>
+        </div>
+</body>
+</html>
